3e90ae49bc8b5c6b46b0fc839b8f77bc4b5527e5
Kaushik9876 <ritikkaushik16@gmail.com>
2024-09-18 10:22:04 +0530

    basic of python

---
+++ file.py
@@ -0,0 +1,18 @@
+# name = input("Enter the name ")
+# collageName = input("Enter the collage ");
+# Email = input("Enter Email")
+
+
+
+# f = open("pankaj.txt","w")
+# f.write(name+collageName+Email)
+# # print(f.readline());
+# f.close();
+
+# # ============Home work=============
+# # -> name, email , collage name 
+# # csv
+# # json
+# # excel
+
+#while print 1 to 10
---
+++ first.py
@@ -0,0 +1,61 @@
+# friendname =["rahul","pankaj","subhash"]
+# print("before", friendname)
+
+# friendname.append("ritik")
+# print("after", friendname)
+
+# friendname.insert(0,"abishek")
+# friendname.insert(3,"abishek")
+# print("add name on index 0 :", friendname)
+
+# friendname.remove("abishek")
+# print(friendname) 
+
+# friendname.pop(3)
+# print(friendname) 
+
+# friendname.sort(reverse = True)
+# print(friendname)
+
+
+# for x in range(1,11):
+#     if(x%2  != 0):
+#         print(x)
+
+# sets = ("rahul", "pankaj", "ritik")
+# sets.add("pankaj")
+# sets.remove("pankaj")
+# sets[0] =("hsgshs")
+# print(sets)
+# print(type(sets))
+
+# friends = ["rahul","ritik","pankaj","subhash","roop"]
+# friends.sort(reverse = True)
+# print(friends)
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.month)
+ 
+
+# i = 10
+# while i>=1:
+    
+
+#     print(i)
+#     i-=1
+
+    # i = 11
+    # while i<11:
+    # i=i+1
+    # print(i)
+    # continue
+width = int(input("enter the width"))
+height = int(input("enter the height"))
+
+def area(width,height):
+    z=width*height
+area(width,height)
+print("the area is", area)
+
+area(width,height)
